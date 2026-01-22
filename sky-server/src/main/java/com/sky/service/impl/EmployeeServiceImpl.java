e2520d5251a0ba3e1d2607a6d90b27466c9ca6a4
YuanXing <1129294076@qq.com>
2026-01-22 15:40:03 +0800

    save employee

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,13 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
+
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -53,6 +59,35 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+
+        // 对象属性拷贝 BeanUtils.copyProperties(src, target) 前提是两者的属性名必须一致
+        BeanUtils.copyProperties(employeeDTO,employee);
+
+        // 设置账号状态，默认启用: 1
+        employee.setStatus(StatusConstant.ENABLE);
+
+        // 设置密码 默认为123456
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        // 创建时间和更新时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        // 设置当前记录创建人id和修改人id。即当前登录用户的id
+        // TODO
+        employee.setCreateUser(10L);// 目前写个假数据，后期修改
+        employee.setUpdateUser(10L);
+
+        employeeMapper.insert(employee);// 后续步骤定义
     }
 
 }
