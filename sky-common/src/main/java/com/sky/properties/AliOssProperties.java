fd014f860000c99bc13745b6761cdea98e208c90
YuanXing <1129294076@qq.com>
2026-01-23 14:17:13 +0800

    文件上传功能

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "sky.alioss")
+@ConfigurationProperties(prefix = "sky.alioss") // 配置属性类，读取配置文件中的配置项，封装成Java对象
 @Data
 public class AliOssProperties {
 
