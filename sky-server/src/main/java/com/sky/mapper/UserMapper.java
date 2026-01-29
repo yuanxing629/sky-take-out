03a86f91deeb02bba620083d48977dd0da1d0bac
YuanXing <1129294076@qq.com>
2026-01-29 17:52:33 +0800

    导入了订单支付功能。

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -9,6 +9,7 @@ public interface UserMapper {
 
     /**
      * 根据openid查询用户
+     *
      * @param openid
      * @return
      */
@@ -17,7 +18,16 @@ public interface UserMapper {
 
     /**
      * 插入数据
+     *
      * @param user
      */
     void insert(User user);
+
+    /**
+     *  根据id查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long userId);
 }
