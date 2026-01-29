1d48cc52ff819eaa7b4d78d7a5966c299807b5e0
YuanXing <1129294076@qq.com>
2026-01-29 15:29:35 +0800

    地址簿功能开发

---
+++ sky-server/src/main/java/com/sky/service/AddressBookService.java
@@ -0,0 +1,52 @@
+package com.sky.service;
+
+import com.sky.entity.AddressBook;
+
+import java.util.List;
+
+public interface AddressBookService {
+
+    /**
+     * 条件查询
+     *
+     * @param addressBook
+     * @return
+     */
+    List<AddressBook> list(AddressBook addressBook);
+
+    /**
+     * 新增地址
+     *
+     * @param addressBook
+     */
+    void save(AddressBook addressBook);
+
+    /**
+     * 修改地址
+     *
+     * @param addressBook
+     */
+    void update(AddressBook addressBook);
+
+    /**
+     * 根据id查询地址
+     *
+     * @param id
+     * @return
+     */
+    AddressBook getById(Long id);
+
+    /**
+     * 设置默认地址
+     *
+     * @param addressBook
+     */
+    void setDefault(AddressBook addressBook);
+
+    /**
+     * 根据id删除地址
+     *
+     * @param id
+     */
+    void deleteById(Long id);
+}
